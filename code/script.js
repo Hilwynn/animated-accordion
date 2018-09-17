a07721fb88038878e0731c887525b827dbfeeaef
Anna Erkers <31205429+Hilwynn@users.noreply.github.com>
2018-09-17 14:50:22 +0200

    Icons and styling
    Added icons + JS functionality to switch between + and -

--- code/script.js
+++ code/script.js
@@ -1,6 +1,13 @@
 // A function that adds and removes the class "active" on the section you click on.
+
 function toggle() {
-  this.classList.toggle("active")
+  //Add or remove the "active" class.
+  this.classList.toggle("active");
+
+  //Toggle between plus and minus icon depending on which one is currently showing
+  var iconEl = this.getElementsByTagName("i");
+  iconEl[0].classList.toggle("fa-plus");
+  iconEl[0].classList.toggle("fa-minus");
 }
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
